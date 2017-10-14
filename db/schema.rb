4808ef3bb283e923c4992504a3975a96d3aaaf44
Safonov88 <safonovdmitr@gmail.com>
2017-10-14 22:14:13 +0300

    background color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007193930) do
+ActiveRecord::Schema.define(version: 20171014183615) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171007193930) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "color"
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["username", "email"], name: "index_users_on_username_and_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
