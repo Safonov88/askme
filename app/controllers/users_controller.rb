17700ce73072be9a0c3b385a0e390df49d836180
Safonov88 <safonovdmitr@gmail.com>
2017-10-15 13:48:32 +0300

    Add user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    redirect_to root_url, notice: 'Пользователь удалён'
+  end
+
   def show
     @questions = @user.questions.order(created_at: :desc)
 
