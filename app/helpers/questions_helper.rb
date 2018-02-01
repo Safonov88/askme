c21343239420d483af6a89af6b12f4e691dfd4c1
Safonov88 <safonovdmitr@gmail.com>
2018-02-01 20:00:30 +0300

    update hashtag helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,5 @@
 module QuestionsHelper
   def hash_tag(text)
-    a = ""
-    a = text.scan(/\B#\w*[a-zA-Zа-яА-Я]+\w*/).to_s unless text.nil?
-    a.delete '[]",'
+    text.scan(/\B#\w*[a-zA-Zа-яА-Я]+\w*/).to_s.delete '[]",' unless text.nil?
   end
 end
